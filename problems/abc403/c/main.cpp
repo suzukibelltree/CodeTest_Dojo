3e8d18c0b5d1d6654e385dea1188b49f82363d15
suzukibelltree <suzuki879455@gmail.com>
2026-08-06 12:47:03 +0900

    更新

---
+++ problems/abc403/c/main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+
+    // ここに解法を書く
+    int n, m, q;
+    cin >> n >> m >> q;
+    int num;
+    while (q--)
+    {
+        cin >> num;
+        if (num == 1)
+        {
+        }
+        else if (num == 2)
+        {
+        }
+        else
+        {
+        }
+    }
+    return 0;
+}
